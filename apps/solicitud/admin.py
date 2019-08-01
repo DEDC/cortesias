fd03634dcc9612c4f39de466f950b3d0c47e6a05
David DC <dedc_011596@hotmail.com>
2019-08-01 00:04:25 -0500

    custom list solictudes

--- apps/solicitud/admin.py
+++ apps/solicitud/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import Eventos, Localidades, Solicitante, Solicitud
 
+class SolicitudesAdmin(admin.ModelAdmin):
+    list_display = ('cantidad','evento', 'localidad', 'solicitante', 'extra')
+    list_display_links = ('evento',)
+    list_filter = ('evento',)
+    ordering = ['evento']
+
 admin.site.register(Eventos)
 admin.site.register(Localidades)
 admin.site.register(Solicitante)
-admin.site.register(Solicitud)
+admin.site.register(Solicitud, SolicitudesAdmin)
 
